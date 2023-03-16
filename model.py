0225b9d5b10ec08a6eb5fec88b4651a17fb912f9
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-16 15:48:24 -0300

    [refactor] extracting functions, improve readability

--- model.py
+++ model.py
@@ -6,13 +6,13 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import statistics
 import numpy as np
-from typing import List
+from typing import List, Tuple
 
 def get_df() -> pd.DataFrame:
     iris = datasets.load_iris(as_frame=True)
     return pd.DataFrame(iris.frame)
 
-def get_test_train(df: pd.DataFrame):
+def get_test_train(df: pd.DataFrame) -> Tuple:
     x_train, x_test, y_train, y_test = train_test_split(df.loc[:, df.columns != 'target'], df['target'], test_size=0.2, random_state=42)
     return x_train, x_test, y_train, y_test
 
@@ -32,25 +32,32 @@ def test_model(df: pd.DataFrame, classifier: KNeighborsClassifier) -> float:
     return 1 - statistics.mean(error)
 
 def save_model(model: KNeighborsClassifier):
-    pickle.dump(model, open('model.pkl', 'wb'))
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
 def load_model() -> KNeighborsClassifier:
-    return pickle.load(open('model.pkl', 'rb'))
+    try:
+        with open('model.pkl', 'rb') as f:
+            model = pickle.load(f)
+        return model
+    except FileNotFoundError:
+        print("File model.pkl does not exist.")
+    except IOError as e:
+        print("An IO error occurred", e)
 
 def main():
     df = get_df()
     classifier = train_model(df)
     save_model(classifier)
     
+def get_iris_species(prediction: int) -> str:
+    return ["Iris-setosa", "Iris-versicolor", "Iris-virginica"][prediction]
 
-def predict(model: KNeighborsClassifier, prompt: List[List]):
-    pred = pd.DataFrame(np.array(prompt).reshape(1, -1), columns=['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)'])
+def preprocess(prompt: List[List]) -> pd.DataFrame:
+    return pd.DataFrame(np.array(prompt).reshape(1, -1), columns=['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)'])
+
+def predict(model: KNeighborsClassifier, prompt: List[List]) -> str:
+    assert len(prompt) == 4, "Length format is incorrect. It should be a 4 float list."
+    pred = preprocess(prompt=prompt)
     prediction = model.predict(pred)[0]
-    if(prediction == 0):
-        return('It is a Iris-setosa')
-    elif prediction == 1:
-        return('It is a Iris-versicolor')
-    elif prediction == 2:
-        return('It is a Iris-virginica')
-
-
+    return get_iris_species(prediction=prediction)
