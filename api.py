9ad37dd8913e8fb8736c80be617bfb0a0e881099
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-16 15:47:07 -0300

    [feat] handling api jwt generating and token checking

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, jsonify, make_response
+from typing import List, Dict
 import jwt
 from functools import wraps
 from model import load_model, predict
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
 
 ai_model = load_model()
+
+def generate_token() -> str:
+    credentials = get_credentials()
+    secret = os.getenv("SECRET")
+    return jwt.encode(credentials, secret)
+
+def get_credentials() -> Dict:
+    return {"login": os.getenv("LOGIN"), "password": os.getenv("PASSWORD")}
 
 def token_required(f):
     @wraps(f)
@@ -14,25 +26,48 @@ def token_required(f):
         if not token:
             return jsonify({'message': 'No token provided'}), 401
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            data = jwt.decode(token, os.getenv("SECRET"), algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Signature expired'}), 401
+            return jsonify({'message': 'Signature expired. Please log in again.'}), 401
         except jwt.InvalidTokenError:
-            return jsonify({'message': 'Invalid token'}), 401
+            return jsonify({'message': 'Invalid token. Please log in again.'}), 401
         return f(data, *args, **kwargs)
     return decorated_function
-
 
 @app.route("/")
 def home():
     return "OK"
 
-@token_required
 @app.route("/predict", methods=['POST'])
-def predict_route():
-    json = request.get_json()
-    prompt = []
-    for item in json:
-        prompt.append(json[item])
+@token_required
+def predict_route(x):
+    data = request.get_json()
+    prompt = format_data(data=data)
+
+    if not is_prompt_format_correct(prompt=prompt):
+        return jsonify({"error": "prompt format is not correct"})
+
     prediction = predict(ai_model, prompt)
-    return prediction
+    return jsonify({"prediction": prediction})
+
+@app.route("/generate-token", methods=['POST'])
+def get_token():
+    correct_credentials = get_credentials()
+    data = request.get_json()
+    print(data)
+    if data != correct_credentials:
+        return jsonify(
+            {"error": "Wrong credentials. Format should be {'login': 'login', 'password', 'password'}"}
+        )
+    return jsonify({"token": generate_token()})
+
+
+def is_prompt_format_correct(prompt: List) -> bool:
+    return True if len(prompt) == 4 else False
+
+def format_data(data: Dict) -> List:
+    try:
+        return [float(data[item]) for item in data]
+    except ValueError:
+        return []
+        
